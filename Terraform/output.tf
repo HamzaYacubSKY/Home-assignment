478e421823cbc4e2a3a2626a41e1eedc407d5965
HamzaYacubSKY <hamza.yacub@sky.uk>
2022-02-28 11:53:04 +0000

    added more outputs for worker nodes

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -10,6 +10,14 @@ output "ansible_worker_2_public_ip" {
     value = "${aws_instance.node01[1].*.public_ip}"
 }
 
+output "ansible_worker_1_public_dns" {
+    value = "${aws_instance.node01[0].*.public_dns}"
+}
+
+output "ansible_worker_2_public_dns" {
+    value = "${aws_instance.node01[1].*.public_dns}"
+}
+
 # output "ansible_master_public_dns" {
 #     value = "${aws_instance.ansible-master.*.public_dns}"
 # }
