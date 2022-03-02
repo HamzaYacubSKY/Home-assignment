750d5a4894dff17fa6eca5c6c3a298e7db353cc6
Hamza Yacub <94060233+HamzaYacubSKY@users.noreply.github.com>
2022-03-02 14:53:10 +0000

    removed unused outputs

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -6,14 +6,6 @@ output "ansible_worker_2_public_ip" {
     value = "${aws_instance.node02.*.public_ip}"
 }
 
-output "ansible_worker_1_public_dns" {
-    value = "${aws_instance.node01.*.public_dns}"
-}
-
-output "ansible_worker_2_public_dns" {
-    value = "${aws_instance.node02.*.public_dns}"
-}
-
 output "load_balancer_dns" {
     value = "${aws_lb.lb01.*.dns_name}"
 }
